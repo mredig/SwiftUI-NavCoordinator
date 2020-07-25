a56a3455bd90e129fcd152b1172456a5a329ebff
Michael Redig <mredig@gmail.com>
2020-07-25 13:07:16 -0500

    initial commit

---
+++ Shared/SwiftUI_NavCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavCoordinatorApp.swift
+//  Shared
+//
+//  Created by Michael Redig on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
